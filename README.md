4793bf63bd2867ab354404268374c896f64c9ff1
SorrowCrow <78910309+SorrowCrow@users.noreply.github.com>
2021-06-07 14:20:38 +0300

    cleanup2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hello
+# vue-verify-dev
 
 ## Project setup
 ```
--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,6 @@ module.exports = {
         }
     },
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/hello/'
+    ? '/vue-verify-dev/'
     : '/'
 };
